983ca78fe1848545c7b4f69feeba89ad3316c412
SeeingDragon <wenhao.qing@foxmail.com>
2022-05-01 22:53:56 +0800

    添加项目文件。

---
+++ model.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include "model.h"
+
+Model::Model(const char* filename) : verts_(), faces_() {
+    std::ifstream in;
+    //打开文件并读入
+    in.open(filename, std::ifstream::in);
+    if (in.fail()) return;
+    std::string line;
+    while (!in.eof()) {
+        std::getline(in, line);
+        std::istringstream iss(line.c_str());
+        char trash;
+        if (!line.compare(0, 2, "v ")) {
+            iss >> trash;
+            Vec3f v;
+            for (int i = 0; i < 3; i++) iss >> v.raw[i];
+            verts_.push_back(v);
+        }
+        else if (!line.compare(0, 2, "f ")) {
+            std::vector<int> f;
+            int itrash, idx;
+            iss >> trash;
+            while (iss >> idx >> trash >> itrash >> trash >> itrash) {
+                idx--; // in wavefront obj all indices start at 1, not zero
+                f.push_back(idx);
+            }
+            faces_.push_back(f);
+        }
+    }
+    std::cerr << "# v# " << verts_.size() << " f# " << faces_.size() << std::endl;
+}
+
+Model::~Model() {
+}
+//获取顶点的数量
+int Model::nverts() {
+    return (int)verts_.size();
+}
+//获取三角形的数量
+int Model::nfaces() {
+    return (int)faces_.size();
+}
+//根据idx获取三角形的三个顶点坐标
+std::vector<int> Model::face(int idx) {
+    return faces_[idx];
+}
+//获取顶点
+Vec3f Model::vert(int i) {
+    return verts_[i];
+}
+
