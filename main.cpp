2570e0eff9029160f350a7975c7dae81be371dd3
SeeingDragon <wenhao.qing@foxmail.com>
2022-05-29 00:44:14 +0800

    课8 最终代码（已修改triangle函数）

--- main.cpp
+++ main.cpp
@@ -23,15 +23,18 @@ TGAImage occl(1024, 1024, TGAImage::GRAYSCALE);
 
 struct ZShader : public IShader
 {
+	mat<4, 3, float> varying_tri;
 	virtual Vec4f vertex(int iface, int nthvert)
 	{
 		Vec4f gl_vertex = embed<4>(model->vert(iface, nthvert));
-		gl_vertex =  Viewport * Projection * ModelView * gl_vertex;
+		gl_vertex = Projection * ModelView * gl_vertex;
+		varying_tri.set_col(nthvert, gl_vertex);
 		return gl_vertex;
 	}
 	virtual bool fragment(Vec3f gl_fragcoord,Vec3f bar,TGAColor& color)
 	{
-		color = TGAColor(255, 255, 255) * ((gl_fragcoord.z)/ 2000.f);
+		//color = TGAColor(255, 255, 255) * ((gl_fragcoord.z)/ 2000.f);
+		color = TGAColor(0, 0, 0);
 		return false;
 	}
 };
@@ -61,13 +64,16 @@ struct uvShader : public IShader
 	}
 };
 
+//该着色器读取自己生成的环境光贴图
 struct AOShader : public IShader
 {
 	mat<2, 3, float> varying_uv;
+	mat<4, 3, float> varying_tri;
 	virtual Vec4f vertex(int iface,int nthvert)
 	{
 		Vec4f gl_vertex = embed<4>(model->vert(iface, nthvert));
-		gl_vertex = Viewport * Projection * ModelView * gl_vertex;
+		gl_vertex = Projection * ModelView * gl_vertex;
+		varying_tri.set_col(nthvert,gl_vertex);
 		varying_uv.set_col(nthvert, model->uv(iface, nthvert));
 		return gl_vertex;
 	}
@@ -82,7 +88,6 @@ struct AOShader : public IShader
 
 
 
-
 //球面均匀取点
 Vec3f rand_point_on_unit_sphere()
 {
@@ -92,6 +97,24 @@ Vec3f rand_point_on_unit_sphere()
 	float phi = acos(2.f * v - 1.f);
 	return Vec3f(sin(phi) * cos(theta), sin(phi) * sin(theta), cos(phi));
 }
+
+float max_elevation_angle(float* zbuffer, Vec2f p, Vec2f dir)
+{
+	float maxangle = 0;
+	for (float t = 0; t < 1000.f; t += 1.)
+	{
+		Vec2f cur = p + dir * t;
+		if (cur.x >= width || cur.y >= height || cur.x < 0 || cur.y < 0) return maxangle;
+
+		float distance = (p - cur).norm();
+		if (distance < 1.f) continue;
+		float elevation = zbuffer[int(cur.x) + int(cur.y) * width] - zbuffer[int(p.x) + int(p.y) * width];
+		maxangle = std::max(maxangle, atanf(elevation / distance));
+		
+	}
+	return maxangle;
+}
+
 
 //argc 是 argument count的缩写，表⽰传⼊main函数的参数个数；
 //argv 是 argument vector的缩写，表⽰传⼊main函数的参数序列或指针，并且第⼀个参数argv[0]⼀定是程序的名称，并且包含了程序所在的完整路径，所以确切的说需要我们输⼊的main函数的参数个数应该是argc - 1个；
@@ -118,81 +141,54 @@ int main(int argc, char** argv)
 	//norm函数return std::sqrt(x * x + y * y + z * z)
 	projection(-1.f / (eye - center).norm());
 
-	Vec4f screen_coords[3];
-
-	//该着色器读取自己生成的环境光贴图
-	AOShader shader;
-
+	/*AOShader shader;
 	for (int i = 0; i < model->nfaces(); i++)
 	{
 		for (int j = 0; j < 3; j++)
 		{
-			screen_coords[j] = shader.vertex(i, j);
+			shader.vertex(i, j);
 		}
-		triangle(screen_coords, shader, frame, zbuffer);
+		triangle(shader.varying_tri, shader, frame, zbuffer);
 	}
 	frame.flip_vertically();
-	frame.write_tga_file("output.tga");
+	frame.write_tga_file("output.tga");*/
 
-
-	//以下代码均匀在球面上生成一千个向量模拟光照，此时眼睛和光照所处位置相同
+	//获得zbuffer
+	ZShader zshader;
+	for (int i = 0; i < model->nfaces(); i++)
 	{
-		const int nrenders = 1;
-		for (int iter = 1; iter <= nrenders; iter++)
+		for (int j = 0; j < 3; j++)
 		{
-			std::cerr << iter << " from" << nrenders << std::endl;
-			for (int i = 0; i < 3; i++) up[i] = (float)rand() / (float)RAND_MAX;
-			eye = rand_point_on_unit_sphere();
-			eye.y = std::abs(eye.y);
-			std::cout << "v " << eye << std::endl;
-
-			for (int i = width * height; i--; zbuffer[i] = shadowbuffer[i] = -std::numeric_limits<float>::max());
-
-			TGAImage frame(width, height, TGAImage::RGB);
-
-			lookat(eye, center, up);
-			viewport(width / 8, height / 8, width * 3 / 4, height * 3 / 4);
-			projection(0);
-
-			//第一次渲染，将深度值存储在shadowbuffer上。
-			ZShader zshader;
-
-			for (int i = 0; i < model->nfaces(); i++)
-			{
-				for (int j = 0; j < 3; j++)
-				{
-					screen_coords[j] = zshader.vertex(i, j);
-				}
-				triangle(screen_coords, zshader, frame, shadowbuffer);
-			}
-			frame.flip_vertically();
-			frame.write_tga_file("framebuffer.tga");
-
-			uvShader shader;
-			occl.clear();
-			for (int i = 0; i < model->nfaces(); i++)
-			{
-				for (int j = 0; j < 3; j++)
-				{
-					screen_coords[j] = shader.vertex(i, j);
-				}
-				triangle(screen_coords, shader, frame, zbuffer);
-			}
-
-			for (int i = 0; i < 1024; i++)
-			{
-				for (int j = 0; j < 1024; j++)
-				{
-					float tmp = total.get(i, j)[0];
-					total.set(i, j, TGAColor((tmp * (iter - 1) + occl.get(i, j)[0]) / (float)iter + 0.5f));
-				}
-			}
+			zshader.vertex(i, j);
 		}
-		total.flip_vertically();
-		total.write_tga_file("occlusion.tga");
-		occl.flip_vertically();
-		occl.write_tga_file("occl.tga");
+		triangle(zshader.varying_tri, zshader, frame, zbuffer);
 	}
+	
+
+	for (int x = 0; x < width; x++)
+	{
+		for (int y = 0; y < height; y++)
+		{
+			//如果该zbuffer[idx]值无限小，则该像素点不被三角形所覆盖，所以直接跳出该循环
+			//1e5 1*10的五次方
+			if (zbuffer[x + y * width] < -1e5) continue;
+			float total = 0;
+			//1e-4 1*10的负4次方
+			//循环八次，减掉1e-4或许是为了误差，保证循环八次，不是七次也不是九次
+			for (float a = 0; a < M_PI * 2 - 1e-4; a += M_PI / 4)
+			{
+				//计算z缓冲区每个点的立体角，但我们将其近似为（90°-max_elevation_angle）/ 8的总和
+				total += M_PI / 2 - max_elevation_angle(zbuffer, Vec2f(x, y), Vec2f(cos(a), sin(a)));
+			}
+			total /= (M_PI / 2) * 8;
+			total = pow(total, 100.f);
+			frame.set(x, y, TGAColor(total * 255, total * 255, total * 255));
+			
+		}
+		std::cout << x << std::endl;
+	}
+	frame.flip_vertically();
+	frame.write_tga_file("framebuffer.tga");
 
 	delete model;
 	delete[] zbuffer;
@@ -201,3 +197,61 @@ int main(int argc, char** argv)
 }
 
 
+//以下代码均匀在球面上生成一千个向量模拟光照，生成环境光贴图。此时眼睛和光照所处位置相同
+	//{
+	//	const int nrenders = 1;
+	//	for (int iter = 1; iter <= nrenders; iter++)
+	//	{
+	//		std::cerr << iter << " from" << nrenders << std::endl;
+	//		for (int i = 0; i < 3; i++) up[i] = (float)rand() / (float)RAND_MAX;
+	//		eye = rand_point_on_unit_sphere();
+	//		eye.y = std::abs(eye.y);
+	//		std::cout << "v " << eye << std::endl;
+
+	//		for (int i = width * height; i--; zbuffer[i] = shadowbuffer[i] = -std::numeric_limits<float>::max());
+
+	//		TGAImage frame(width, height, TGAImage::RGB);
+
+	//		lookat(eye, center, up);
+	//		viewport(width / 8, height / 8, width * 3 / 4, height * 3 / 4);
+	//		projection(0);
+
+	//		//第一次渲染，将深度值存储在shadowbuffer上。
+	//		ZShader zshader;
+
+	//		for (int i = 0; i < model->nfaces(); i++)
+	//		{
+	//			for (int j = 0; j < 3; j++)
+	//			{
+	//				screen_coords[j] = zshader.vertex(i, j);
+	//			}
+	//			triangle(screen_coords, zshader, frame, shadowbuffer);
+	//		}
+	//		frame.flip_vertically();
+	//		frame.write_tga_file("framebuffer.tga");
+
+	//		uvShader shader;
+	//		occl.clear();
+	//		for (int i = 0; i < model->nfaces(); i++)
+	//		{
+	//			for (int j = 0; j < 3; j++)
+	//			{
+	//				screen_coords[j] = shader.vertex(i, j);
+	//			}
+	//			triangle(screen_coords, shader, frame, zbuffer);
+	//		}
+
+	//		for (int i = 0; i < 1024; i++)
+	//		{
+	//			for (int j = 0; j < 1024; j++)
+	//			{
+	//				float tmp = total.get(i, j)[0];
+	//				total.set(i, j, TGAColor((tmp * (iter - 1) + occl.get(i, j)[0]) / (float)iter + 0.5f));
+	//			}
+	//		}
+	//	}
+	//	total.flip_vertically();
+	//	total.write_tga_file("occlusion.tga");
+	//	occl.flip_vertically();
+	//	occl.write_tga_file("occl.tga");
+	//}
